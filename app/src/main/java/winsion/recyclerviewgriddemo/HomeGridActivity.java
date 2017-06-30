c95f82b755aa6428ee8353d8b207d7d2b6fcbebc
dys <289133385@qq.com>
2017-06-30 16:41:08 +0800

    add type

--- app/src/main/java/winsion/recyclerviewgriddemo/HomeGridActivity.java
+++ app/src/main/java/winsion/recyclerviewgriddemo/HomeGridActivity.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,10 +28,12 @@ public class HomeGridActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat);
-        for (int i = 0; i < 8; i++) {
-            if (i == 0) {
-                mList.add(new HomeGrid(HomeGridAdapter.TYPE_LONG, R.mipmap.ic_launcher, null, -1));
-            } else if (i == 5) {
+        for (int i = 0; i < 10; i++) {
+            if (i < 2) {
+                mList.add(new HomeGrid(HomeGridAdapter.TYPE_WHOLE, R.mipmap.ic_launcher, null, -1));
+            } else if (i == 2) {
+                mList.add(new HomeGrid(HomeGridAdapter.TYPE_LONG, R.mipmap.ic_launcher, null, getResources().getColor(R.color.colorPrimary)));
+            } else if (i == 7) {
                 mList.add(new HomeGrid(HomeGridAdapter.TYPE_NORMAL_IMG, R.mipmap.ic_launcher, "Y", getResources().getColor(R.color.colorPrimaryDark)));
             } else {
                 mList.add(new HomeGrid(HomeGridAdapter.TYPE_NORMAL, R.mipmap.ic_launcher_round, i + "", getResources().getColor(R.color.colorAccent)));
@@ -55,6 +56,7 @@ public class HomeGridActivity extends AppCompatActivity {
         public static final int TYPE_LONG = 1;
         public static final int TYPE_NORMAL = 2;
         public static final int TYPE_NORMAL_IMG = 3;
+        public static final int TYPE_WHOLE = 4;
 
         public HomeGridAdapter(Context context, List<HomeGrid> list) {
             mContext = context;
@@ -64,25 +66,27 @@ public class HomeGridActivity extends AppCompatActivity {
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            if (viewType == TYPE_LONG || viewType == TYPE_NORMAL_IMG) {
-                return new ViewHolderLong(mLayoutInflater.inflate(R.layout.item_home_img, parent, false));
-            } else {
+            if (viewType == TYPE_NORMAL) {
                 return new ViewHolderNormal(mLayoutInflater.inflate(R.layout.item_home_normal, parent, false));
+            } else {
+                return new ViewHolderLong(mLayoutInflater.inflate(R.layout.item_home_img, parent, false));
             }
         }
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
             int type = getItemViewType(position);
-            if (type == TYPE_LONG || type == TYPE_NORMAL_IMG) {
-                ViewHolderLong holderLong = (ViewHolderLong) holder;
-                holderLong.mImageView.setImageResource(mList.get(position).getIconPicId());
-
-            } else {
+            if (type == TYPE_NORMAL) {
                 ViewHolderNormal holderNormal = (ViewHolderNormal) holder;
                 holderNormal.ivIcon.setImageResource(mList.get(position).getIconPicId());
                 holderNormal.tvName.setText(mList.get(position).getIconName());
                 holderNormal.llRoot.setBackgroundColor(mList.get(position).getIconBgColor());
+            } else {
+                ViewHolderLong holderLong = (ViewHolderLong) holder;
+                holderLong.mImageView.setImageResource(mList.get(position).getIconPicId());
+                if (mList.get(position).getIconBgColor() != -1){
+                    holderLong.mImageView.setBackgroundColor(mList.get(position).getIconBgColor());
+                }
             }
         }
 
@@ -101,18 +105,21 @@ public class HomeGridActivity extends AppCompatActivity {
             super.onAttachedToRecyclerView(recyclerView);
             RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
             if (manager instanceof GridLayoutManager) {
-                GridLayoutManager gridLayoutManager = (GridLayoutManager) manager;
+                final GridLayoutManager gridLayoutManager = (GridLayoutManager) manager;
                 gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
                     @Override
                     public int getSpanSize(int position) {
                         int type = getItemViewType(position);
+                        if (type == TYPE_WHOLE) {
+                            return gridLayoutManager.getSpanCount();
+                        }
                         if (type == TYPE_LONG) {
                             return 4;
                         }
                         if (type == TYPE_NORMAL) {
                             return 2;
                         }
-                        if (type == TYPE_NORMAL_IMG){
+                        if (type == TYPE_NORMAL_IMG) {
                             return 2;
                         }
                         return 2;
